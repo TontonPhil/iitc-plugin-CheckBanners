e9a1d23f820bb5ee186716cb879fbc40bb5bc928
Philippe <p>
2018-08-28 17:21:31 +0200

    Initial commit

---
+++ CheckBanners.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @id             iitc-plugin-CheckBanners
+// @name           IITC plugin: Check Banners availability
+// @author         TontonPhil
+// @category       Layer
+// @version        0.0.2.2018.08.28.01
+// @namespace      https://github.com/TontonPhil/iitc-plugin-CheckBanners
+// @updateURL      https://github.com/TontonPhil/iitc-plugin-CheckBanners/raw/master/CheckBanners.meta.js
+// @downloadURL    https://github.com/TontonPhil/iitc-plugin-CheckBanners/raw/master/CheckBanners.js
+// @description    search and check if a banner is still present (detect missing portals)
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
