c4c299cc029ebb27fe2385e0b0240e51c0106dc6
TontonPhil <tonton.phil@gmail.com>
2019-10-20 20:36:08 +0200

    0.0.3 change display for missing portals + add draw path

--- CheckBanners.meta.js
+++ CheckBanners.meta.js
@@ -3,10 +3,10 @@
 // @name           IITC plugin: Check Banners availability
 // @author         TontonPhil
 // @category       Layer
-// @version        0.0.3.2019.10.20.01
+// @version        0.0.3.2019.10.20.02
 // @namespace      https://github.com/TontonPhil/iitc-plugin-CheckBanners
 // @updateURL      https://github.com/TontonPhil/iitc-plugin-CheckBanners/raw/master/CheckBanners.meta.js
-// @downloadURL    https://github.com/TontonPhil/iitc-plugin-CheckBanners/raw/master/CheckBanners.js
+// @downloadURL    https://github.com/TontonPhil/iitc-plugin-CheckBanners/raw/master/CheckBanners.user.js
 // @description    search and check if a banner is still present (detect missing portals)
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
