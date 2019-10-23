bc64326295950a55c1107ffabf145fd66c9788fa
TontonPhil <tonton.phil@gmail.com>
2019-10-23 21:41:05 +0200

    0.0.3.1 try to fix issue with previous link still pointing to https://github.com/TontonPhil/iitc-plugin-CheckBanners/raw/master/CheckBanners.js

---
+++ CheckBanners.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @id             iitc-plugin-CheckBanners
+// @name           IITC plugin: Check Banners availability
+// @author         TontonPhil
+// @category       Layer
+// @version        0.0.3.2019.10.20.02
+// @namespace      https://github.com/TontonPhil/iitc-plugin-CheckBanners
+// @updateURL      https://github.com/TontonPhil/iitc-plugin-CheckBanners/raw/master/CheckBanners.meta.js
+// @downloadURL    https://github.com/TontonPhil/iitc-plugin-CheckBanners/raw/master/CheckBanners.user.js
+// @description    search and check if a banner is still present (detect missing portals)
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
